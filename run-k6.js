81be723aa96ae190932bcf3c0f738106fe15bb78
vasanthsdet <dama.vasanth@gmail.com>
2024-07-26 19:18:49 +0530

    First commit

---
+++ run-k6.js
@@ -0,0 +1,11 @@
+const { exec } = require('child_process');
+
+exec('k6 run scripts/test-script.js', (err, stdout, stderr) => {
+    if (err) {
+        console.error(`Error executing k6 script: ${err}`);
+        return;
+    }
+    console.log(`k6 stdout: ${stdout}`);
+    console.error(`k6 stderr: ${stderr}`);
+});
+
---
+++ scripts/test-script.js
@@ -0,0 +1,30 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+import { Rate } from 'k6/metrics';
+
+// Define custom metrics
+let errorRate = new Rate('errors');
+
+export let options = {
+    vus: 10, // Number of virtual users
+    duration: '30s', // Duration of the test
+    thresholds: {
+        'http_req_duration': ['p(95)<500'], // 95% of requests should be below 500ms
+        'errors': ['rate<0.01'], // Error rate should be less than 1%
+    },
+};
+
+export default function () {
+    let res = http.get('https://test-api.k6.io/public/crocodiles/1/');
+    
+    // Check for successful response
+    let success = check(res, {
+        'status is 200': (r) => r.status === 200,
+    });
+
+    // Track errors
+    errorRate.add(!success);
+
+    sleep(1);
+}
+
